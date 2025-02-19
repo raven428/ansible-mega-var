ff5fd0ff19c56715dd2f5afb781cf03e75b63121
Dmitry Sukhodoyev <raven428@gmail.com>
2025-02-19 19:18:25 +0500

    update molecule tests

--- molecule/run-tests.sh
+++ molecule/run-tests.sh
@@ -2,9 +2,14 @@
 set -ueo pipefail
 umask 0022
 : "${CONT_NAME:="ans2dkr-${USER}"}"
+: "${IMAGE_NAME:="ansible-11_1_0:latest"}"
 : "${SSH_AUTH_SOCK:="/dev/null"}"
 MY_BIN="$(readlink -f "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
+[[ -v ANSIBLE_GITHUB_TOKEN ]] || {
+  echo 'mandatory variable [ANSIBLE_GITHUB_TOKEN] not found, exiting…'
+  exit 1
+}
 export DEBIAN_FRONTEND=noninteractive
 /usr/bin/env which sponge >/dev/null || {
   /usr/bin/env sudo apt-get update &&
@@ -18,7 +23,7 @@ export DEBIAN_FRONTEND=noninteractive
 [[ "${SSH_AUTH_SOCK}" == "/dev/null" ]] && export SSH_AUTH_SOCK
 mkdir -vp "${HOME}"/.{ansible_async,cache}
 ANSIBLE_CONT_NAME="${CONT_NAME}"
-ANSIBLE_IMAGE_NAME='ghcr.io/raven428/container-images/docker-ansible-11_1_0:latest'
+ANSIBLE_IMAGE_NAME="ghcr.io/raven428/container-images/docker-${IMAGE_NAME}"
 export ANSIBLE_CONT_NAME ANSIBLE_IMAGE_NAME
 {
   cd "${MY_PATH}/.."
@@ -40,14 +45,18 @@ while ! /usr/bin/env docker exec "${CONT_NAME}" systemctl status docker; do
   sleep 1
 done
 for action in create check converge; do
-  printf "\n\n\n"
-  echo "molecule [$action] action"
+  printf "\n\n\nmolecule [%s] action\n" $action
   ansible-docker.sh molecule -v ${action}
 done
-printf "\n\n\n"
-echo "molecule [converge] check"
+printf "\n\n\nmolecule [converge] check\n"
 ansible-docker.sh molecule -v converge -- --check --extra-vars post_dem=1
-printf "\n\n\n"
-echo "molecule [idempotence] check"
+printf "\n\n\nmolecule [idempotence] check\n"
 ansible-docker.sh molecule -v idempotence -- --check
+
+printf "\n\n\nmolecule [update] action\n"
+ANSIBLE_DOWNLOAD_TAG='001' ansible-docker.sh molecule -v converge
+printf "\n\n\nmolecule [update] check\n"
+ANSIBLE_DOWNLOAD_TAG='001' ansible-docker.sh molecule -v check
+printf "\n\n\nmolecule [idempotence] update\n"
+ANSIBLE_DOWNLOAD_TAG='001' ansible-docker.sh molecule -v idempotence -- --check
 #/usr/bin/env docker rm -f "${CONT_NAME}"
