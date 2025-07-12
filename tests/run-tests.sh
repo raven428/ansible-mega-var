a86a4865aa6c7996b018ea5c7d4c8761c7987f35
Dmitry Sukhodoyev <raven428@gmail.com>
2025-07-12 19:44:48 +0500

    test commit

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -6,5 +6,6 @@ MY_PATH="$(dirname "${MY_BIN}")"
 cd "${MY_PATH}/.."
 # shellcheck disable=1091
 SKIP_DID=1 source "${MY_PATH}/../molecule/prepare.sh"
+
 ansible-docker.sh pytest --color=yes \
   -vv -o cache_dir=/tmp/meva_pro/main tests/meva_pro/main_tests.py
