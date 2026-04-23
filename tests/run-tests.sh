0620074bf789b84af3e071668b8b069af6e3544d
Dmitry Sukhodoev <raven428@gmail.com>
2026-04-23 14:30:22 +0000

    update GitHub workflow

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 umask 0022
-MY_BIN="$(readlink -f "$0")"
+MY_BIN="$(realpath "$0")"
 MY_PATH="$(dirname "${MY_BIN}")"
 cd "${MY_PATH}/.."
 # shellcheck disable=1091
 SKIP_DID=1 source "${MY_PATH}/../molecule/prepare.sh"
-ansible-docker.sh pytest --color=yes \
+# shellcheck disable=2154
+"${_appimage_bin}" pytest --color=yes \
   -vv -o cache_dir=/tmp/meva_pro/main tests/meva_pro/main_tests.py
